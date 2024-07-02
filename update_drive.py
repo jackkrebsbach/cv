201faebba7275407cbb002a13e3717954c9b3cb5
Jack Krebsbach <krebsbachjackson@gmail.com>
2024-07-02 11:10:50 -0400

    take out script

---
+++ update_drive.py
@@ -0,0 +1,38 @@
+import os
+import json
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
+
+# Load credentials
+creds_json = os.environ['GOOGLE_DRIVE_CREDENTIALS']
+creds_dict = json.loads(creds_json)
+creds = service_account.Credentials.from_service_account_info(creds_dict)
+
+# Create Drive API client
+drive_service = build('drive', 'v3', credentials=creds)
+
+# Search for the file
+response = drive_service.files().list(
+    q="name='cv.pdf' and 'root' in parents",
+    spaces='drive',
+    fields='files(id, name)'
+).execute()
+
+files = response.get('files', [])
+
+if not files:
+    print('File not found. Creating new file.')
+    file_metadata = {'name': 'cv.pdf'}
+    media = MediaFileUpload('cv.pdf', resumable=True)
+    file = drive_service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+    print(f'File ID: {file.get("id")}')
+else:
+    file_id = files[0]['id']
+    print(f'Updating existing file with ID: {file_id}')
+    media = MediaFileUpload('cv.pdf', resumable=True)
+    updated_file = drive_service.files().update(
+        fileId=file_id,
+        media_body=media
+    ).execute()
+    print(f'File updated. ID: {updated_file.get("id")}')
